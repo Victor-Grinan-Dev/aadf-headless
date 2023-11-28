d7d0c137d8392c9c01798eb72b03edbf0dbfcf5d
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-11-28 09:50:00 +0200

    installed scss

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
+import Content from "./components/Content";
+
+function App() {
+  console.log(process.env.REACT_APP_DISCOURSE_API_KEY); //not working!
+
+  const headers = {
+    "Content-Type": "application/json",
+    "Api-Key":
+      "71f97d255bcdfbac20e6cd85d037131e4757ff097f8cc5bf793ae94fc605edae",
+  };
+  return (
+    <div className="App">
+      <Navbar />
+      <Banner />
+      <Content />
+    </div>
+  );
+}
+
+export default App;
