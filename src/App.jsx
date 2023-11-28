c2c14bdce67762240052ccf218c616a16ddb8932
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-11-28 11:17:24 +0200

    navbar startert

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
 import Content from "./components/Content";
+import Footer from "./components/Footer";
 
 function App() {
   console.log(process.env.REACT_APP_DISCOURSE_API_KEY); //not working!
@@ -15,6 +16,7 @@ function App() {
       <Navbar />
       <Banner />
       <Content />
+      <Footer />
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import logo from "../assets/logo4.png";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="navbar_logo-container">
+        <div>
+          <img src={logo} alt="app-logo" className="navbar_app-logo" />
+        </div>
+        <div className="navbar_title">all About dog food</div>
+      </div>
+      <div className="navbar_menu-container">
+        <div className="navbar_hamburger navbar_hamburger-top"></div>
+        <div className="navbar_hamburger navbar_hamburger-mid"></div>
+        <div className="navbar_hamburger navbar_hamburger-bot"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
